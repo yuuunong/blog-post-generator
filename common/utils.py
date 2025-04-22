b156761c60bbe5c58c0e2b71f6dde81af1f55031
yuuunong <ccbb15379@naver.com>
2025-04-22 11:25:22 +0900

    aws에서 langchain api key 추가

--- common/utils.py
+++ common/utils.py
@@ -8,8 +8,17 @@ def __set_api_key():
 
     if not GROQ_API_KEY:
         ssm = boto3.client('ssm')
+
+        # Groq API Key
         parameter = ssm.get_parameter(Name='/CICD/BLOG-POST-GENERATOR/GROQ_API_KEY', WithDecryption=True)
         os.environ['GROQ_API_KEY'] = parameter['Parameter']['Value']
+
+        # Langchain API Key
+        parameter = ssm.get_parameter(Name='/CICD/BLOG-POST-GENERATOR/LANGCHAIN_API_KEY', WithDecryption=True)
+        os.environ['LANGCHAIN_API_KEY'] = parameter['Parameter']['Value']
+        os.environ['LANGCHAIN_PROJECT'] = 'blog-post-generator'
+        os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+        os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
 
 
 def init_chatbot():
